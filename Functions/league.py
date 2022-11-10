02b4fa5275a5ec5bfc475ffbafd16a0af67760be
Cheng <lemonsoldout@163.com>
2022-11-10 17:28:35 -0500

    :poop: info command done | match command bugs needs fix

--- Functions/league.py
+++ Functions/league.py
@@ -24,50 +24,87 @@ def getLeagueAccountByName(name):
     "Origin": "https://developer.riotgames.com",
     "X-Riot-Token": "RGAPI-e7a8a20f-6f25-4471-a7c8-b34ad5a73066"}
     
-    
-    
     url = f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}'
     
-    
-    # data = {'grant_type':'','username': str(username),"password": str(username),'scope':'','client_id':'','client_secret':''}
-            
-    #res = requests.post(url=url,params=data,headers=headers)
-    res = requests.get(url=url,headers=headers)
-    return res.json()
+    res = requests.get(url=url,headers=headers).json()
+    if 'status' in res and res['status']['status_code'] == 404:
+        return "Target Account Not Found"
+    return res
 
 
-
-def getLeagueAccountRankInfo(acc_id):
+def getLeagueAccountRankInfo(summoner_id):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35",
     "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
     "Origin": "https://developer.riotgames.com",
     "X-Riot-Token": "RGAPI-e7a8a20f-6f25-4471-a7c8-b34ad5a73066"}
     
-    url = f'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/{acc_id}'
+    url = f'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/{summoner_id}'
     # url = 'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/q9dWDdaT6Ctll-XnUecP1ubtwQSAu4qjA9UHv2V2LjRHgBA'
     
-    res = requests.get(url=url,headers=headers)
-    
-    return res.json()
+    res = requests.get(url=url,headers=headers).json()
+    if res == []:
+        return "Unranked"
+    return res
 
-
-def showTargetAccRankInfo(name):
-    if 'status' in getLeagueAccountByName(name) and getLeagueAccountByName(name)['status']['status_code'] == 404:
-        return "Target Account Not Found"
+def getCurrentMatchPlayers(name):
+    res = getLeagueAccountByName(name)
+    if "Target Account Not Found" in res:
+        return res
+    else:
+        summoner_id = res['id']
+        
     
-    acc_id = getLeagueAccountByName(name)["id"]
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35",
+        "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Origin": "https://developer.riotgames.com",
+        "X-Riot-Token": "RGAPI-e7a8a20f-6f25-4471-a7c8-b34ad5a73066"
+    }
+    url=f"https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{summoner_id}"
+    
+    
+    res = requests.get(url=url,headers=headers).json()
+    if 'status' in res and res['status']['message'] == 'Data not found':
+        return "Target player is not in a game"
+    
+    json_players = res['participants']
+    players = []
+    for i in json_players:
+        players.append(i['summonerName'])
+    return players
+
+def getCurrentMatchInfo():
+    pass
+
+def getTargetPlayerHistoryGames():
+    pass
+
+##############################################################
+def displayTargetAccRankInfo(name):
+    res = getLeagueAccountByName(name)
+    if "Target Account Not Found" in res:
+        return res
+    else:
+        summoner_id = res["id"]
     result = {}
-    # result['id'] = acc_id
+    # result['id'] = summoner_id
+    rankInfo = getLeagueAccountRankInfo(summoner_id)
+    result["用户名"] = name
+    result['等级'] = res['summonerLevel']
     
-    result["用户名"] = getLeagueAccountRankInfo(acc_id)[0]["summonerName"]
-    result['等级'] = getLeagueAccountByName(name)['summonerLevel']
-    # if getLeagueAccountRankInfo(acc_id)[0]['inactive'] == False:
+    if 'Unranked' in rankInfo:
+        return result
+    
+    
+    
+    # if rankInfo['inactive'] == False:
     #     result["状态"] = "离线"
     # else:
     #     result["状态"] = "在线"
     
-    for i in getLeagueAccountRankInfo(acc_id):
+    for i in rankInfo:
         if i['tier'] == "IRON":
             tier = "<:lol_rank1_iron:1039995313370443816>"
         elif i['tier'] == "BRONZE":
@@ -87,7 +124,13 @@ def showTargetAccRankInfo(name):
         elif i['tier'] == "CHALLENGER":
             tier = "<:lol_rank9_challenger:1039995774349615198>"
         else:
-            tier = "UNRANKED"  
+            tier = "Unranked"  
+        # if 'queueType' not in i:
+        #     result["FLEX"] = {}
+        #     result["FLEX"]["灵活"] = "Unranked"
+        #     result["SOLO"] = {}
+        #     result["SOLO"]["单双"] = "Unranked"
+            
         if "FLEX" in i['queueType']:
             result["FLEX"] = {}    
             result["FLEX"]['灵活'] = f"{tier} {i['tier']} {i['rank']}  {i['leaguePoints']} 分"
@@ -104,7 +147,7 @@ def showTargetAccRankInfo(name):
             win_rate = round(i['wins']/(i['losses'] + round(i['wins'])),2) * 100
             result["SOLO"]["胜率"] = f"{win_rate} %"
     
-            # if {getLeagueAccountRankInfo(acc_id)[0]['hotStreak']} == True:
+            # if {rankInfo[0]['hotStreak']} == True:
             #     result["SOLO"]["是否连续上分"] = "是"
             # else:
             #     result["SOLO"]["是否连续上分"] = "否"
@@ -112,8 +155,19 @@ def showTargetAccRankInfo(name):
             return "N/A"
     
     
-    # # result["老兵"] = f"{getLeagueAccountRankInfo(acc_id)[0]['veteran']}"
-    # # result["新兵蛋子"] = f"{getLeagueAccountRankInfo(acc_id)[0]['freshBlood']}"
+    # # result["老兵"] = f"{rankInfo[0]['veteran']}"
+    # # result["新兵蛋子"] = f"{rankInfo[0]['freshBlood']}"
     
     return result
+##############################################################
+def displayCurrentMatchPlayersInfo(name):
+    players = getCurrentMatchPlayers(name)
+    result = {}
+    for i in players:
+        result[i] = displayTargetAccRankInfo(i)
 
+    return result
+##############################################################
+# print(getCurrentMatchPlayers("UtopiaZeta"))
+# print()
+#print(displayCurrentMatchPlayersInfo("Maple Bookmark"))
